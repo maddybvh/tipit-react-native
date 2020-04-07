3a1d4c3fbee528d5c479877e97a4e39ee5d6c017
maddybvh <maddybvh@gmail.com>
2020-04-07 17:54:26 -0400

    Add results object

--- App.js
+++ App.js
@@ -6,14 +6,14 @@ import RangeSlider from 'react-native-range-slider';
 export default function App() {
     const list = [
       {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President'
+        bill: '$100',
+        tip: '$20',
+        total: '$120',
       },
       {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman'
+        bill: '$50',
+        tip: '$10',
+        total: '$60',
       },
     ]
     
@@ -36,9 +36,9 @@ export default function App() {
                     list.map((l, i) => (
                       <ListItem
                         key={i}
-                        leftAvatar={{ source: { uri: l.avatar_url } }}
-                        title={l.name}
-                        subtitle={l.subtitle}
+                        leftElement={l.bill}
+                        title={l.tip}
+                        rightElement={l.total}
                         bottomDivider
                       />
                     ))
