706e3a5c4705c36d8ce412084a2ec4b4f04f7632
maddybvh <maddybvh@gmail.com>
2020-04-07 16:57:10 -0400

    Adds a list under the inputs

--- App.js
+++ App.js
@@ -4,8 +4,21 @@ import { Input, ListItem} from 'react-native-elements';
 import RangeSlider from 'react-native-range-slider';
 
 export default function App() {
+    const list = [
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+    ]
+    
   return (
-            <View style={styles.container}>
+            <View >
                 <Input
                   placeholder='Bill'
                   leftIcon={{ type: 'font-awesome', name: 'dollar' }}
@@ -18,6 +31,20 @@ export default function App() {
                   placeholder='Tip high'
                   rightIcon={{ type: 'font-awesome', name: 'percent' }}
                 />
+                <View>
+                  {
+                    list.map((l, i) => (
+                      <ListItem
+                        key={i}
+                        leftAvatar={{ source: { uri: l.avatar_url } }}
+                        title={l.name}
+                        subtitle={l.subtitle}
+                        bottomDivider
+                      />
+                    ))
+                  }
+                </View>
+
             </View>
 
   );
