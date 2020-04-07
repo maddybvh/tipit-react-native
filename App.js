56ae02b4fba7ffe465b9c8facd3936e2e5908cfc
maddybvh <maddybvh@gmail.com>
2020-04-07 15:57:02 -0400

    Attempted to add react elements

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, ListItem} from 'react-native-elements';
+import RangeSlider from 'react-native-range-slider';
 
 export default function App() {
   return (
