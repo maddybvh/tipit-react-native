7612e64026ee7484b585e5532fbf139bd2944455
maddybvh <maddybvh@gmail.com>
2020-04-22 11:06:38 -0400

    Style table results

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,22 +5,26 @@ import {
   FlatList,
   StyleSheet,
   Text,
+  View,
 } from 'react-native';
 import Constants from 'expo-constants';
 
 function Item({ id, bill, tip, total, selected, onSelect }) {
   return (
-    <TouchableOpacity
-    //   onPress={}
-      style={[
-        styles.item,
-        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
-      ]}
-    >
-      <Text style={styles.title}>${bill}</Text>
-      <Text style={styles.title}>${tip}</Text>
-      <Text style={styles.title}>${total}</Text>
-    </TouchableOpacity>
+      
+        <TouchableOpacity 
+            //   onPress={}
+            style={[
+                styles.item
+            ]}
+            >
+                <View {...{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                    <Text style={styles.results}>${bill} + </Text>
+                    <Text style={styles.results}>${tip}</Text>
+                    <Text style={styles.total}>${total}</Text>
+                </View>
+        </TouchableOpacity>
+    
   );
 }
 
@@ -44,17 +48,29 @@ export default class Results extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
+    item: {
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 10,
+    },
+    tableHead: {
+        fontFamily: 'JetBrainsMono-Bold',
+        fontSize: 14,
+        lineHeight: 16,
+        },
+    results: {
+        fontFamily: 'JetBrainsMono-Regular',
+        fontSize: 18,
+        },
+    total: {
+        fontFamily: 'JetBrainsMono-Regular',
+        fontSize: 18,
+        flex: 2,
+        textAlign: 'right',
+        },
+    }
+);
