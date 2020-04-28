46cd2807815c9faaf699eff960f7e5b1a307352e
maddybvh <maddybvh@gmail.com>
2020-04-28 08:42:44 -0400

    Results themed by context

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,18 +3,20 @@ import { TouchableOpacity, FlatList, StyleSheet, Text, View, Clipboard } from 'r
 import UserContext from './UserContext';
 
 function Item({ id, bill, tip, total, selected, onSelect }) {
+    const context = useContext(UserContext)
+    const { colors } = context.useTheme2()
     return (      
         <TouchableOpacity 
             onPress={() => {
                 Clipboard.setString(tip);
                 alert('Copied your $' + tip + ' tip to the clipboard. Happy tipping :)');
             }}
-            style={styles.item}
+            style={[styles.item, {backgroundColor: colors.row}]}
             >
             <View {...{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}}>
-                <Text style={styles.results}>${bill} + </Text>
-                <Text style={styles.results}>${tip}</Text>
-                <Text style={styles.total}>${total}</Text>
+                <Text style={[styles.results, {color: colors.text}]}>${bill} + </Text>
+                <Text style={[styles.results, {color: colors.text}]}>${tip}</Text>
+                <Text style={[styles.total, {color: colors.text}]}>${total}</Text>
             </View>
         </TouchableOpacity>
   );
@@ -38,9 +40,9 @@ export const Results = ({results}) =>  {
                 ListHeaderComponent={
                     results.length > 0 &&
                         <View style={styles.tableHead}>
-                            <Text style={styles.tableHead}>Bill</Text>
-                            <Text style={styles.tableHead}>Tip</Text>
-                            <Text style={[styles.tableHead, {textAlign: 'right'}]}>Total</Text>
+                            <Text style={[styles.tableHead, {color: colors.text}]}>Bill</Text>
+                            <Text style={[styles.tableHead, {color: colors.text}]}>Tip</Text>
+                            <Text style={[styles.tableHead, {textAlign: 'right', color: colors.text}]}>Total</Text>
                         </View>
                     }
             />
