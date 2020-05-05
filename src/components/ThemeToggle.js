7a703b461b93a46a1f807e222937bfee302dfbb0
maddybvh <maddybvh@gmail.com>
2020-05-05 11:16:56 -0400

    Set intial theme toggle state based on saved user settings

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -4,8 +4,11 @@ import AppContext from './AppContext';
 
 export default function ThemeToggle() {
   const context = useContext(AppContext)
+  const theme = context.theme;
+
+
   
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(theme == 'light' ? false : true);
   const toggleSwitch = () => {
     setIsEnabled(previousState => !previousState);
     context.toggleTheme();
