6bd2afb7bd79c5953b1bbad93bd07dade91d2ee1
maddybvh <maddybvh@gmail.com>
2020-05-05 10:51:07 -0400

    Store user settings in async storage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, SafeAreaView, AsyncStorage } from 'react-native';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import { Header } from './src/components/Header';
@@ -9,6 +9,38 @@ import AppContext, { useTheme } from './src/components/AppContext';
 import { themedColors } from './src/theme/index'
 
 export default function App () {
+  const _storeData = async () => {
+    try {
+      await AsyncStorage.removeItem('@store:appContext')
+      settingsToSave = {
+        defaultTipLow: userSettings.defaultTipLow,
+        defaultTipHigh: userSettings.defaultTipHigh,
+        theme: userSettings.theme,
+      }
+      await AsyncStorage.setItem('@store:appContext', JSON.stringify(settingsToSave));
+      console.log('Stored data = ' + JSON.stringify(settingsToSave) )
+    } catch (error) {
+      console.log('Error saving user settings.')
+    }
+  };
+  
+  const _retrieveData = async () => {
+    try {
+      const value = await JSON.parse(AsyncStorage.getItem('@store:appContext'));
+      if (value.theme !== null) {
+        savedSettings = {
+          defaultTipLow: value.defaultTipLow,
+          defaultTipHigh: value.setDefaultTipHigh,
+          theme: value.theme,
+        }
+        return savedSettings
+      }
+    } catch (error) {
+      console.log('No user setting data was retrieved');
+    }
+  };
+
+  let savedSettings = _retrieveData();
   
   let [fontsLoaded] = useFonts({
     'JetBrainsMono-Regular': require('./assets/fonts/JetBrainsMono-Regular.ttf'),
@@ -21,15 +53,15 @@ export default function App () {
   const [theme, setTheme] = useState('light');
 
   const updateTipLowContext = (input) => {
-    setDefaultTipLow(input)
+    setDefaultTipLow(input);   
   }
 
   const updateTipHighContext = (input) => {
-    setDefaultTipHigh(input)
+    setDefaultTipHigh(input);
   }
 
   const toggleTheme = () => {
-    theme == 'light' ? setTheme('dark') : setTheme('light')
+    theme == 'light' ? setTheme('dark') : setTheme('light');
   }
 
   const userSettings = { 
@@ -44,6 +76,10 @@ export default function App () {
   
   //This is a repeat of useTheme() in /AppContext
   const colors = userSettings.theme ? themedColors[userSettings.theme] : themedColors.default
+
+  useEffect(() => {
+    _storeData()
+  })
 
   if (!fontsLoaded) {
     return <AppLoading />;
