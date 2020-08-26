d8e560d172afa4692bd2d3a39104a91a2cd79179
maddybvh <maddybvh@gmail.com>
2020-08-26 10:23:20 -0400

    Issue #27032495: Handle no results

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,20 @@
-import React, { useContext, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { CurrencyInput } from './CurrencyInput';
-import { PercentInput } from './PercentInput';
-import { Dashes } from './Dashes';
-import { Results } from './Results';
-import AppContext from './AppContext';
+import React, { useContext, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { CurrencyInput } from "./CurrencyInput";
+import { PercentInput } from "./PercentInput";
+import { Dashes } from "./Dashes";
+import { Results } from "./Results";
+import AppContext from "./AppContext";
 
 let resultID = 0;
 
 //Determine if a string is a palindrome
 function palindrome(str) {
-    var re = '.';
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr
-      .split('')
-      .reverse()
-      .join('');
-    return reverseStr === lowRegStr;
-  }
+  var re = ".";
+  var lowRegStr = str.toLowerCase().replace(re, "");
+  var reverseStr = lowRegStr.split("").reverse().join("");
+  return reverseStr === lowRegStr;
+}
 
 // Returns an array of palindromes between numLow and numHigh
 function palindromeArray(numLow, numHigh) {
@@ -38,7 +35,7 @@ function findPalTips(billAmount, tipPercentLow, tipPercentHigh) {
     billAmount * tipPercentHigh * 0.01
   );
   return tipArray;
-  }
+}
 
 //Create an array with all totals with palindrome tips
 function arrayTipsAndTotals(billAmount, tipArray) {
@@ -49,8 +46,13 @@ function arrayTipsAndTotals(billAmount, tipArray) {
     let total = Number(i) + Number(billAmount);
     let stringTip = i.toFixed(2).toString();
     let stringTotal = total.toFixed(2).toString();
-    var result = new Result (resultID.toString(), stringBill, stringTip, stringTotal)
-    results.push(result)
+    var result = new Result(
+      resultID.toString(),
+      stringBill,
+      stringTip,
+      stringTotal
+    );
+    results.push(result);
     resultID += 1;
   }
   return results;
@@ -60,59 +62,62 @@ function arrayTipsAndTotals(billAmount, tipArray) {
 function findPalTotals(billAmount, tipArray) {
   let i;
   var results = [];
-  let stringBill = billAmount.toFixed(2).toString()
+  let stringBill = billAmount.toFixed(2).toString();
   for (i of tipArray) {
-      let total = Number(billAmount) + Number(i);
-      let stringTotal = total.toFixed(2).toString()
-      let stringTip = i.toFixed(2).toString();
-      if (palindrome(stringTotal)) {
-          const result = new Result (resultID.toString(), stringBill, stringTip, stringTotal)
-          results.push(result)
-      }
-      resultID += 1;
+    let total = Number(billAmount) + Number(i);
+    let stringTotal = total.toFixed(2).toString();
+    let stringTip = i.toFixed(2).toString();
+    if (palindrome(stringTotal)) {
+      const result = new Result(
+        resultID.toString(),
+        stringBill,
+        stringTip,
+        stringTotal
+      );
+      results.push(result);
+    }
+    resultID += 1;
   }
-  return results
+  return results;
 }
 
-function Result (id, bill, tip, total) {
-    this.id = id
-    this.bill = bill
-    this.tip = tip
-    this.total = total
+function Result(id, bill, tip, total) {
+  this.id = id;
+  this.bill = bill;
+  this.tip = tip;
+  this.total = total;
 }
-
 
 export const Calculator = () => {
-  const context = useContext(AppContext)
-  const { colors } = context.useTheme()
-  
+  const context = useContext(AppContext);
+  const { colors } = context.useTheme();
+
   const [tipLow, setTipLow] = useState(context.defaultTipLow);
   const [tipHigh, setTipHigh] = useState(context.defaultTipHigh);
-  const [bill, setBill] = useState('');
+  const [bill, setBill] = useState("");
   const [results, setResults] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
-  
-  async function handleBill (text) {
-    await setBill(text)
+  async function handleBill(text) {
+    await setBill(text);
     setResults(findResults());
   }
-  async function handleTipLow (text) {
+  async function handleTipLow(text) {
     await setTipLow(text);
     setResults(findResults());
   }
-  async function handleTipHigh (text) {
-    await setTipHigh(text)
+  async function handleTipHigh(text) {
+    await setTipHigh(text);
     setResults(findResults());
   }
   clearTable = () => {
     setResults([]);
-    setMessage('');
-  }
+    setMessage("");
+  };
   clearAll = () => {
-    setBill('');
+    setBill("");
     clearTable();
-  }
+  };
 
   //Update the state based on the functions above
   findResults = () => {
@@ -120,111 +125,133 @@ export const Calculator = () => {
     const tipLowFloat = parseFloat(tipLow);
     const tipHighFloat = parseFloat(tipHigh);
 
-    if (billFloat && tipLowFloat && tipHighFloat && tipLowFloat <= tipHighFloat){
-
+    if (
+      billFloat &&
+      tipLowFloat &&
+      tipHighFloat &&
+      tipLowFloat <= tipHighFloat
+    ) {
       const tipArray = findPalTips(billFloat, tipLowFloat, tipHighFloat);
       const allTotalArray = arrayTipsAndTotals(billFloat, tipArray);
       const palTotalArray = findPalTotals(billFloat, tipArray);
-      
-      if (palTotalArray && palTotalArray.length) { //both tip & total are arrays
-          setMessage('Woot! There are ' + palTotalArray.length + ' ways for the tip AND total to be palindromes!');
-          setResults(palTotalArray);
-          return palTotalArray
-      } else if (tipArray && tipArray.length) {
-          setMessage('You can tip in ' + tipArray.length + ' palindromes!');
-          setResults(allTotalArray);
-          return allTotalArray
+
+      if (palTotalArray && palTotalArray.length) {
+        //both tip & total are arrays
+        setMessage(
+          "Woot! There are " +
+            palTotalArray.length +
+            " ways for the tip AND total to be palindromes!"
+        );
+        setResults(palTotalArray);
+        return palTotalArray;
+      } else if (tipArray && tipArray.length > 0) {
+        setMessage("You can tip in " + tipArray.length + " palindromes!");
+        setResults(allTotalArray);
+        return allTotalArray;
+      } else {
+        setMessage("Sorry, no results.");
+        return [];
       }
     }
-    else return []
-  }
+  };
 
   return (
-    <View>                          
-        <View style={styles.inputRow}>
-          <View style={{flex: 1}}>
-            <Text style={[styles.label, {color: colors.text}]}>Your Bill:</Text>
-            <Text style={[styles.helper, {color: colors.text}]}>Pre-tip amount</Text>
-          </View>
-          <CurrencyInput style={{flex: 2}}
-            label='Bill'
-            value={bill}
-            onChange={handleBill}
+    <View>
+      <View style={styles.inputRow}>
+        <View style={{ flex: 1 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Your Bill:</Text>
+          <Text style={[styles.helper, { color: colors.text }]}>
+            Pre-tip amount
+          </Text>
+        </View>
+        <CurrencyInput
+          style={{ flex: 2 }}
+          label="Bill"
+          value={bill}
+          onChange={handleBill}
+        />
+      </View>
+      <View style={styles.inputRow}>
+        <View>
+          <Text style={[styles.label, { color: colors.text }]}>Tip Range:</Text>
+          <Text style={[styles.helper, { color: colors.text }]}>
+            Low to high
+          </Text>
+        </View>
+        <View style={styles.inputGroup}>
+          <PercentInput
+            defaultValue={context.defaultTipLow}
+            onChange={handleTipLow}
+          />
+          <Text style={[styles.normalText, { margin: 7, color: colors.text }]}>
+            to
+          </Text>
+          <PercentInput
+            defaultValue={context.defaultTipHigh}
+            onChange={handleTipHigh}
           />
         </View>
-        <View style={styles.inputRow}>
-            <View>
-              <Text style={[styles.label, {color: colors.text}]}>Tip Range:</Text>
-              <Text style={[styles.helper, {color: colors.text}]}>Low to high</Text>
-            </View>
-            <View style={styles.inputGroup}>
-                <PercentInput
-                    defaultValue={context.defaultTipLow}
-                    onChange={handleTipLow}
-                />
-                <Text style={[styles.normalText, {margin:7, color: colors.text}]}>to</Text>
-                <PercentInput
-                    defaultValue={context.defaultTipHigh}
-                    onChange={handleTipHigh}
-                />                
-            </View>
+      </View>
+      <View style={{ minHeight: 50 }}>
+        {/* If there are results, print the message and clear button. */}
+        {results && (
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text style={[styles.normalText, { flex: 3, color: colors.text }]}>
+              {message}
+            </Text>
+            <TouchableOpacity
+              onPress={clearAll}
+              style={{ flex: 1, justifyContent: "flex-start" }}
+            >
+              <Text style={[styles.clearButton, { color: colors.clear }]}>
+                X Clear
+              </Text>
+            </TouchableOpacity>
           </View>
-          <View style={{minHeight: 50}}>
-            {/* If there are results, print the message and clear button. */}
-            {results.length > 0 &&
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Text 
-                      style={[styles.normalText, {flex: 3, color: colors.text}]}>
-                      {message}
-                    </Text>
-                    <TouchableOpacity onPress={clearAll} style={{flex: 1, justifyContent: 'flex-start'}}>
-                        <Text style={[styles.clearButton, {color: colors.clear}]}>X Clear</Text>
-                    </TouchableOpacity>
-                </View>
-                }
-        </View>
-        <View >
-          <Dashes />
-          <Results results={results}/>
-        </View>               
+        )}
+      </View>
+      <View>
+        <Dashes />
+        <Results results={results} />
+      </View>
     </View>
-    );
-  }
+  );
+};
 
 const styles = StyleSheet.create({
   inputRow: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginVertical: 12,
-  }, 
+  },
   label: {
-    fontFamily: 'JetBrainsMono-Regular', 
+    fontFamily: "JetBrainsMono-Regular",
     fontSize: 18,
     lineHeight: 24,
   },
   helper: {
-    fontFamily: 'JetBrainsMono-Italic', 
+    fontFamily: "JetBrainsMono-Italic",
     fontSize: 10,
     lineHeight: 14,
   },
   normalText: {
-    fontFamily: 'JetBrainsMono-Regular', 
+    fontFamily: "JetBrainsMono-Regular",
     fontSize: 12,
     lineHeight: 20,
     marginTop: 10,
   },
   clearButton: {
-      fontFamily: 'JetBrainsMono-Regular',
-      fontSize: 12,
-      lineHeight: 14,
-      color: '#FF0000',
-      textAlign: 'right',
-      padding: 12,
+    fontFamily: "JetBrainsMono-Regular",
+    fontSize: 12,
+    lineHeight: 14,
+    color: "#FF0000",
+    textAlign: "right",
+    padding: 12,
   },
   inputGroup: {
     flexDirection: "row",
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     marginTop: -25,
-  }
+  },
 });
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,79 +1,93 @@
-import React, { useContext } from 'react';
-import { TouchableOpacity, FlatList, StyleSheet, Text, View, Clipboard } from 'react-native';
-import AppContext from './AppContext';
+import React, { useContext } from "react";
+import {
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Clipboard,
+} from "react-native";
+import AppContext from "./AppContext";
 
 function Item({ bill, tip, total }) {
-    const context = useContext(AppContext)
-    const { colors } = context.useTheme()
-    return (      
-        <TouchableOpacity 
-            onPress={() => {
-                Clipboard.setString(tip);
-                alert('Copied your $' + tip + ' tip to the clipboard. Happy tipping :)');
-            }}
-            style={[styles.item, {backgroundColor: colors.row}]}
-            >
-            <View {...{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}}>
-                <Text style={[styles.results, {color: colors.text}]}>${bill} + </Text>
-                <Text style={[styles.results, {color: colors.text}]}>${tip}</Text>
-                <Text style={[styles.total, {color: colors.text}]}>${total}</Text>
-            </View>
-        </TouchableOpacity>
+  const context = useContext(AppContext);
+  const { colors } = context.useTheme();
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        Clipboard.setString(tip);
+        alert(
+          "Copied your $" + tip + " tip to the clipboard. Happy tipping :)"
+        );
+      }}
+      style={[styles.item, { backgroundColor: colors.row }]}
+    >
+      <View
+        {...{ flex: 1, flexDirection: "row", justifyContent: "space-evenly" }}
+      >
+        <Text style={[styles.results, { color: colors.text }]}>${bill} + </Text>
+        <Text style={[styles.results, { color: colors.text }]}>${tip}</Text>
+        <Text style={[styles.total, { color: colors.text }]}>${total}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
-
-export const Results = ({results}) =>  {    
-    const context = useContext(AppContext)
-    const { colors } = context.useTheme()
-    return (
-        <FlatList 
-            data={results}
-            renderItem={({ item, index }) => (
-                <Item
-                    bill={item.bill}
-                    tip={item.tip}
-                    total={item.total}
-                />               
-            )}
-            keyExtractor={item => item.id}
-            ListHeaderComponent={
-                results.length > 0 &&
-                    <View style={styles.tableHead}>
-                        <Text style={[styles.tableHead, {color: colors.text}]}>Bill</Text>
-                        <Text style={[styles.tableHead, {color: colors.text}]}>Tip</Text>
-                        <Text style={[styles.tableHead, {textAlign: 'right', color: colors.text}]}>Total</Text>
-                    </View>
-                }
-        />
-    );
-}
+export const Results = ({ results }) => {
+  const context = useContext(AppContext);
+  const { colors } = context.useTheme();
+  return (
+    <FlatList
+      data={results}
+      renderItem={({ item, index }) => (
+        <Item bill={item.bill} tip={item.tip} total={item.total} />
+      )}
+      keyExtractor={item => item.id}
+      ListHeaderComponent={
+        results &&
+        results.length > 0 && (
+          <View style={styles.tableHead}>
+            <Text style={[styles.tableHead, { color: colors.text }]}>Bill</Text>
+            <Text style={[styles.tableHead, { color: colors.text }]}>Tip</Text>
+            <Text
+              style={[
+                styles.tableHead,
+                { textAlign: "right", color: colors.text },
+              ]}
+            >
+              Total
+            </Text>
+          </View>
+        )
+      }
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 10,
-        marginVertical: 8,
-        marginHorizontal: 10,
-        borderRadius: 3,
-    },
-    tableHead: {
-        fontFamily: 'JetBrainsMono-Bold',
-        fontSize: 14,
-        lineHeight: 16,
-        marginHorizontal: 10,
-        flex: 1, 
-        flexDirection: 'row', 
-        justifyContent: 'space-evenly',
-        },
-    results: {
-        fontFamily: 'JetBrainsMono-Regular',
-        fontSize: 18,
-        },
-    total: {
-        fontFamily: 'JetBrainsMono-Regular',
-        fontSize: 18,
-        flex: 2,
-        textAlign: 'right',
-        },
-    }
-);
+  item: {
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 10,
+    borderRadius: 3,
+  },
+  tableHead: {
+    fontFamily: "JetBrainsMono-Bold",
+    fontSize: 14,
+    lineHeight: 16,
+    marginHorizontal: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  results: {
+    fontFamily: "JetBrainsMono-Regular",
+    fontSize: 18,
+  },
+  total: {
+    fontFamily: "JetBrainsMono-Regular",
+    fontSize: 18,
+    flex: 2,
+    textAlign: "right",
+  },
+});
